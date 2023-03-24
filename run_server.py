3f297f9d079865cf50ed94917a06117b5f93c982
SmartWashingMachine <metaloctopus1438@outlook.com>
2023-03-24 15:34:38 +1100

    Add proper pytorch import.

--- run_server.py
+++ run_server.py
@@ -21,6 +21,11 @@ import sklearn.metrics._pairwise_distances_reduction
 import unidic_lite
 from unidic_lite import unidic
 
+try:
+    import torch
+except:
+    pass
+
 from gandy.app import run_server
 
 run_server()
